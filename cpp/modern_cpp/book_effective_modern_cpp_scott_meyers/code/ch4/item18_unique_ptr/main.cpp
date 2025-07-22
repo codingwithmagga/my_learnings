f925ae119237823ba2dc07dceb37504214fc8318
codingwithmagga <codingwithmagga@gmail.com>
2025-07-22 14:35:10 +0200

    Add item 18

---
+++ cpp/modern_cpp/book_effective_modern_cpp_scott_meyers/code/ch4/item18_unique_ptr/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+class Investment {
+public:
+    virtual ~Investment() { }
+};
+
+class Stock : public Investment {
+public:
+    Stock()
+    {
+        std::cout << "Stock class created\n";
+    }
+};
+
+class Bond : public Investment {
+public:
+    Bond()
+    {
+        std::cout << "Bond class created\n";
+    }
+};
+
+class RealEstate : public Investment {
+public:
+    RealEstate()
+    {
+        std::cout << "RealEstate class created\n";
+    }
+};
+
+enum class InvestmentType { Stock,
+    Bond,
+    RealEstate };
+
+template <typename... Ts>
+auto makeInvestment(InvestmentType type, Ts&&... params)
+{
+    auto delInvmt = [](Investment* pInvestment) {
+        std::cout << "Delete Investment\n";
+        delete pInvestment;
+    };
+
+    std::unique_ptr<Investment, decltype(delInvmt)>
+        pInv(nullptr, delInvmt);
+
+    if (type == InvestmentType::Stock) {
+        pInv.reset(new Stock(std::forward<Ts>(params)...));
+    } else if (type == InvestmentType::Bond) {
+        pInv.reset(new Bond(std::forward<Ts>(params)...));
+    } else if (type == InvestmentType::RealEstate) {
+        pInv.reset(new RealEstate(std::forward<Ts>(params)...));
+    }
+
+    return pInv;
+}
+
+int main()
+{
+    auto investment1 = makeInvestment(InvestmentType::Stock);
+    auto investment2 = makeInvestment(InvestmentType::Bond);
+    auto investment3 = makeInvestment(InvestmentType::RealEstate);
+}
